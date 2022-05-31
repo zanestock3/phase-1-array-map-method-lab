3852cf478ec5e29d9e07b48b0a432d4b2735820c
Zane Stock <zanestock3@gmail.com>
2022-05-31 07:28:23 -0500

    commit tree 4e51a5fbbc02159c70e0335e5d16f763c5ddac55

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(tutorials) {
+  for (let i = 0; i < tutorials.length; i++) {
+    array[i] = array[i].split(" ").map(function (word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  }
+  return
 }
